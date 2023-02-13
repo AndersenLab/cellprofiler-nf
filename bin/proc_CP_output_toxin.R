256396049be606028f1216bd9707a5e71feed4d4
tcrombie <tim.crombie@gmail.com>
2023-02-13 12:39:17 -0600

    add primary object measures to output

--- bin/proc_CP_output_toxin.R
+++ bin/proc_CP_output_toxin.R
@@ -1,4 +1,4 @@
-# /usr/bin/Rscript
+#!/usr/bin/env Rscript
 library(fs)
 library(dplyr)
 library(tidyr)
@@ -12,6 +12,7 @@ library(purrr)
 # Arguments
 #==============================================================================#
 # 1 - out directory path
+#args <- c("/projects/b1059/projects/Tim/cellprofiler-nf/debug/20220501_toxinDebug/Analysis-20230126")
 args <- commandArgs(trailingOnly = TRUE)
 
 #==============================================================================#
@@ -20,14 +21,23 @@ args <- commandArgs(trailingOnly = TRUE)
 # get the output for each model
 dir <- glue::glue("{args[1]}/processed_data")
 
+# get the wormobject data too
+PrimaryObjects <- readr::read_csv(file = dir %>% fs::dir_ls(regexp = "WormObjects\\.csv$")) %>%
+  dplyr::select(FileName_RawBF, ObjectNumber, AreaShape_Area:AreaShape_Solidity) %>%
+  rename_all(.vars = 2:27, ~ paste0("po_", .x))
+
 # read in files and manipulate with  
-model_df <- dir %>%
-  fs::dir_ls(regexp = "\\.csv$") %>% # find paths to csvs in dir
+model_df_raw <- dir %>%
+  fs::dir_ls(regexp = "_NonOverlappingWorms\\.csv$") %>% # find paths to csvs in dir
   purrr::map_dfr(readr::read_csv, .id = "model") %>%
   dplyr::mutate(Metadata_Date = as.integer(Metadata_Date), 
-                model = stringr::str_remove(fs::path_file(as.character(model)), pattern = ".csv"),
+                model = stringr::str_remove_all(fs::path_file(as.character(model)), pattern = "_NonOverlappingWorms|.csv"),
                 model = paste0(model, ".model.outputs")) %>%
   dplyr::arrange(model, ImageNumber)
+
+# join the model outputs with the primary object shape data
+model_df <- model_df_raw %>%
+  dplyr::left_join(PrimaryObjects, by = c("FileName_RawBF" = "po_FileName_RawBF", "Parent_WormObjects" = "po_ObjectNumber"))
 
 # split to list
 model_df_list <- split.data.frame(model_df, model_df$model)
@@ -38,7 +48,7 @@ lapply(seq_along(model_df_list), function(i) assign(names(model_df_list)[i], mod
 # save as R.data
 proj_name <- stringr::str_extract(args[1], pattern = "[^\\/]+(?=(?:\\/[^\\/]+){1}$)")
 run_stamp <- stringr::str_extract(args[1], pattern = "([^/]+$)")
-save(list = c(ls(pattern = "model.outputs")),
+save(list = c(ls(pattern = "model.outputs|wormobj")),
      file = glue::glue("{args[1]}/processed_data/{proj_name}_{run_stamp}.RData"))
 
 # clean up extra CP_outputs for now
--- main.nf
+++ main.nf
@@ -308,6 +308,7 @@ process proc_CP_output_dauer {
 process proc_CP_output_toxin {
 
     //publishDir "${params.out}/processed_data", mode: 'copy', pattern: "*.RData"
+    //publishDir "${params.out}/processed_data", mode: 'copy'
     
     input:
         tuple val(cp_output), val(out_dir), val(model_name1), val(model_name2),
@@ -322,18 +323,17 @@ process proc_CP_output_toxin {
         mkdir ${out_dir}/processed_data
         if [ -d ${out_dir}/processed_images ]; then rm -Rf ${out_dir}/processed_images; fi
         mkdir ${out_dir}/processed_images
+        
         # find .csv files, concatenate them, and write new file
         find ${out_dir}/CP_output -type f -name '${model_name1}.csv' -print0 | xargs -0 awk 'FNR>1 || NR==1 {print}' > ${out_dir}/processed_data/${model_name1}.csv
-        
-        # find .csv files, concatenate them, and write new file
         find ${out_dir}/CP_output -type f -name '${model_name2}.csv' -print0 | xargs -0 awk 'FNR>1 || NR==1 {print}' > ${out_dir}/processed_data/${model_name2}.csv
-        # find .csv files, concatenate them, and write new file
         find ${out_dir}/CP_output -type f -name '${model_name3}.csv' -print0 | xargs -0 awk 'FNR>1 || NR==1 {print}' > ${out_dir}/processed_data/${model_name3}.csv
-        # find .csv files, concatenate them, and write new file
         find ${out_dir}/CP_output -type f -name '${model_name4}.csv' -print0 | xargs -0 awk 'FNR>1 || NR==1 {print}' > ${out_dir}/processed_data/${model_name4}.csv
+        find ${out_dir}/CP_output -type f -name 'WormObjects.csv' -print0 | xargs -0 awk 'FNR>1 || NR==1 {print}' > ${out_dir}/processed_data/WormObjects.csv
         
-        # move all the output images to process_images directory END WITH /?
+        # move all the output images to process_images directory
         find ${out_dir}/CP_output -name '*.png' -exec mv {} ${out_dir}/processed_images \\;
+        
         # Process the CellProfiler output with proc_CP_output.R
         Rscript --vanilla ${proc_CP_out_script} ${out_dir}
 
